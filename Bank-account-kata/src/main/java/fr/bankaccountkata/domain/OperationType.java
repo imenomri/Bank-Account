c8e02588746509214b0e05f203afe0570172cc3b
omrii <omri.imenn@gmail.com>
2021-06-14 00:42:17 +0200

    Bank Account First Commit

---
+++ Bank-account-kata/src/main/java/fr/bankaccountkata/domain/OperationType.java
@@ -0,0 +1,14 @@
+package fr.bankaccountkata.domain;
+
+
+public enum OperationType {
+
+    DEPOSIT("deposit"),
+    WITHDRAWAL("withdrawal");
+
+    String operation;
+
+    OperationType(String operation) {
+        this.operation = operation;
+    }
+}
---
+++ Bank-account-kata/src/main/java/fr/bankaccountkata/utils/NoSuchAccountException.java
@@ -0,0 +1,12 @@
+package fr.bankaccountkata.utils;
+
+public class NoSuchAccountException extends Exception {
+
+    String message;
+
+
+    public NoSuchAccountException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
